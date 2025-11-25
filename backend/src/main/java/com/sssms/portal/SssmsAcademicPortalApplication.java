60b53763ddf63ac4bd0b4a400fb8364e748eb2dd
rush4n <privaterushan@gmail.com>
2025-11-26 01:20:40 +0530

    Initial setup with Docker

---
+++ backend/src/main/java/com/sssms/portal/SssmsAcademicPortalApplication.java
@@ -0,0 +1,13 @@
+package com.sssms.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SssmsAcademicPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SssmsAcademicPortalApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/sssms/portal/SssmsAcademicPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sssms.portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SssmsAcademicPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
